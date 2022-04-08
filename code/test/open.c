b8bd2fc1e1efded3e65b5e87a90e38e5c217129f
nhhung1810 <nghung224@gmail.com>
2022-04-08 18:07:07 +0700

    test file for open

---
+++ code/test/open.c
@@ -0,0 +1,21 @@
+/* readString.c
+ *
+ *	Simple program to test reading a string
+ *
+ */
+
+#include "syscall.h"
+
+int main()
+{
+  // char str[256];
+  // ReadString(str, 255);
+  // PrintString("\nRead string is: ");
+  // PrintString(str);
+  // PrintString("\n");
+  // Create("hung.txt");
+  Open("hung.txt");
+  // PrintNum(id);
+  Halt();
+  /* not reached */
+}
